028501054d673cde249f4d42734606312f3f7507
digiorganics <sunchitsharma8@gmail.com>
2023-11-11 15:55:55 +0000

    Updated icons & api path

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "1bc22ab5b7e5cb8fc42b59007e1bda89",
-"index.html": "b0ccbdaa2c308935965c128d2e736b4b",
-"/": "b0ccbdaa2c308935965c128d2e736b4b",
-"main.dart.js": "7117efbe943f5057a45f0357d7f6439b",
+"index.html": "97314777ed48a3c5531ac1901eca7439",
+"/": "97314777ed48a3c5531ac1901eca7439",
+"main.dart.js": "9a7702a3f401e6e6c4cecf8635844a12",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
